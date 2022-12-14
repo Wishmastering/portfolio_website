87da1c49de7836a280e2d889e41b9832f7ffa73f
Wishmastering <charsdb@gmail.com>
2022-12-14 04:04:55 -0600

    Mas margins, responsive(cols) y detalles CSS agregados

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Footer(){
     return<>
         <div className="d-flex w-100 mt-4 row">
-                <div className="col-xl-4 col-lg-12 col-md-12 col-sm-12 ms-5 me-5 text-center justify-content-center">       
+                <div className="col-xl-3 col-lg-12 col-md-12 col-sm-12 me-3 text-center justify-content-center">       
                         <h1 className="heavyFont fontWeight"> Contact Information —</h1>
                         <p className="">Feel free to reach out to me any time. 
                         I prefer to talk over email, especially 
@@ -11,20 +11,20 @@ export default function Footer(){
                         <p>e:<span className="pointer">  Charsdb@gmail.com</span></p>
                         <p>p:<span className="pointer">  +506 62502302</span></p>  
                 </div>
-
-                <div className="col-xl-2 col-md-3 col-sm-12 ms-3 me-3">
+                
+                <div className="col-xl-3 col-md-3 col-sm-12 ms-2">
                     <h1 className="heavyFont fontWeight"> Latest Projects —</h1>
                     <p className="pointer"><Link href="/projects/where2day">Where2day </Link> </p>
                     <p className="pointer"><Link href="/projects/star_wars"> Starwars Blog </Link> </p>
                     <p className="pointer"><Link href="/"> Play with me!</Link> </p>
                 </div>
-                <div className="col-xl-2 col-md-3 col-sm-12 ms-3 me-3">
+                <div className="col-xl-3 col-md-3 col-sm-12 me-2">
                     <h1 className="heavyFont fontWeight"> Current Availability — </h1>
                     <p>I usually work on several projects but I’ll be happy to discuss new opportunities. 
                         Let’s get in touch!</p>
                 </div>
-                <div className="col-xl-1 col-md-2 col-sm-12 ms-3 me-3 mb-1">
-                    <h1 className="heavyFont fontWeight"> Follow Me On —</h1>
+                <div className="col-xl-2 col-md-2 col-sm-12 ms-3 me-3 mb-1">
+                    <h1 className="heavyFont fontWeight"> Follow Me —</h1>
                     <Link href="https://www.linkedin.com/in/carlos-i-carranza/" target="_blank" className="pointer pointerFoot"><p className="pointer pointerFoot">Linkedin</p></Link>
                     <Link href="https://github.com/Wishmastering" target="_blank"><p className="pointer pointerFoot">Github</p></Link>
                     
